c6d70e68a8cc78c104f9a9e67d0c901459b7f2f4
alxgarcia <Migato07>
2015-03-15 20:42:44 +0100

    MongoDB changes

--- exampleJUnit/src/main/java/com/packtpublishing/tddjava/ch02friendships/Friendships.java
+++ exampleJUnit/src/main/java/com/packtpublishing/tddjava/ch02friendships/Friendships.java
@@ -3,7 +3,7 @@ package com.packtpublishing.tddjava.ch02friendships;
 import java.util.*;
 
 public class Friendships {
-    Map<String, List<String>> friendships = new HashMap<>();
+    private final Map<String, List<String>> friendships = new HashMap<>();
 
     public void makeFriends(String person1, String person2) {
         addFriend(person1, person2);
